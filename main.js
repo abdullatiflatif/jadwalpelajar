e408058767e22827c3a019b8d1c5b97c47853a4e
Abdul latif25 <alatiff045@gmail.com>
2024-08-07 09:33:21 +0700

    menambahfirabase main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdgAITXTldockRB_wgxOrbNUPKzSVBhDs",
+  authDomain: "insan-cemerlang-c9554.firebaseapp.com",
+  projectId: "insan-cemerlang-c9554",
+  storageBucket: "insan-cemerlang-c9554.appspot.com",
+  messagingSenderId: "753628555075",
+  appId: "1:753628555075:web:7a72b2d1e8ae89716931f6",
+  measurementId: "G-KMJZ5V0B8H"
+};
